e43f19e8506b5f1e8d36f86805c28b770e68546e
KhylleVillasurda <freakyvader@proton.me>
2026-05-17 21:52:10 +0800

    chore(config): set the useFakeBackend to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000',
   // Set to false when your real backend (node-mysql-api) is running
-  useFakeBackend: true,
+  useFakeBackend: false,
 };
